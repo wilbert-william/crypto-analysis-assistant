b4e581de98467ba9ee7def01375a2f0e9997c2e6
Wilbert William <126400377+wilbert-william@users.noreply.github.com>
2025-02-25 17:56:52 +0700

    Update api.py

--- tools/api.py
+++ tools/api.py
@@ -4,13 +4,11 @@ import os
 
 class API():
     def __init__(self):
-        # Load environment variables from .env file
         dotenv.load_dotenv()
         
-        # Mengakses API key dari .env
         self.cmc_api_key = os.environ["CMC_API_KEY"]
         self.llm = os.environ["LLM_API"]
-        self.model = 'gemini-2.0-flash'
+        self.model = os.environ["MODEL"]
 
     def cmc_api(self):
         return self.cmc_api_key
