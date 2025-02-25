b9e11972c554a6a6670610758816f2d6aa260af3
Wilbert William <126400377+wilbert-william@users.noreply.github.com>
2025-02-25 18:53:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,5 +57,4 @@ async def chat(request: ChatRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
